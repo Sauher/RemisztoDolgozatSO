13a7e8c5730facecacf8842559852ccedf605ebd
Sauher <schaueroliver@turr.hu>
2025-03-14 10:07:04 +0100

    Add project files.

---
+++ RemisztoDolgozatSO/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemisztoDolgozatSO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string Nev = nev.Text;
+            string Szuldatum = szuldatum.Text;
+            string Telefonszam = telefonszam.Text;
+            string Email = email.Text;
+            NameCardWindow nameCardWindow = new NameCardWindow(Nev, Szuldatum, Telefonszam, Email);
+            nameCardWindow.ShowDialog();
+        }
+
+        private void colors_Click(object sender, RoutedEventArgs e)
+        {
+            ColorSliderWindow colorSliderWindow = new ColorSliderWindow();
+            colorSliderWindow.ShowDialog();
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ RemisztoDolgozatSO/NameCardWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RemisztoDolgozatSO
+{
+    /// <summary>
+    /// Interaction logic for NameCardWindow.xaml
+    /// </summary>
+    public partial class NameCardWindow : Window
+    {
+        public NameCardWindow(string nev, string szuldatum, string teloszam, string email)
+        {
+            InitializeComponent();
+            Feltoltes(nev, szuldatum, teloszam, email);
+        }
+
+        private void Feltoltes(string nev, string szuldatum, string teloszam, string email)
+        {
+            cardName.Text = nev;
+            cardBirthDate.Text = szuldatum;
+            cardPhoneNumber.Text = teloszam;
+            cardEmail.Text = email;
+        }
+    }
+}
